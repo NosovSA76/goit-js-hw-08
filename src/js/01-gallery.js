e33341926c7cfaf328e0b7b0e11138b5c3fbc6a5
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-03-25 12:11:08 +0200

    Cheking

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// // Add imports above this line
+ import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+const imagesMarkup = galleryItems.map(({ original, preview, description }) =>
+  `<li class="gallery__item">
+    <a href="${original}" target="_blank">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+    </a>
+  </li>`
+).join('');
+
+gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import Vimeo from "@vimeo/player";
+var throttle = require('lodash.throttle');
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo(iframe);
+
+ player.on('timeupdate', player.getCurrentTime().then(function(seconds) {
+    console.log(seconds)
+}).catch(function(error) {
+    // an error occurred
+}))
+
+
+// const onPlay = function (data) {
+//   localStorage.setItem('videoplayer-current-time', seconds.data);
+
+//  };
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+//  const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+// console.log(currentTime)
+//  player.setCurrentTime(currentTime)
