7cfa68b5ac72a0e35be3259fdea07a544479cb5e
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-03-28 07:48:53 +0300

    Update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,14 +6,23 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
 
+// player.on('timeupdate', throttle(currentTime, 1000));
+// localStorage.setItem('videoplayer-current-time', player.getCurrentTime(seconds));
+// function currentTime() {
+//     localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds));
+// };
+
 const getCurrentTime = function (currentTime) {
-  if (currentTime.seconds < 60) {
+//   // if (currentTime.seconds < 60) {
     const seconds = currentTime.seconds;
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
-  } else { currentTime.seconds = 58 }
+    localStorage.setItem('videoplayer-current-time', seconds);
+//   // } else { player.setCurrentTime(58) }
 };
+
+
 
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0);
+
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')));
 
